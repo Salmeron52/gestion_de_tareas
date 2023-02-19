f0f2540e301b9750d69332d3402944503064bad6
Salmeron52 <64307573+Salmeron52@users.noreply.github.com>
2023-02-19 23:48:50 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace gestion_de_tareas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabla = new System.Windows.Forms.DataGridView();
+            this.Hora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Actividades = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(4, 1);
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 39);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tabla
+            // 
+            this.tabla.AllowUserToAddRows = false;
+            this.tabla.AllowUserToDeleteRows = false;
+            this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Hora,
+            this.Actividades});
+            this.tabla.Location = new System.Drawing.Point(18, 274);
+            this.tabla.Name = "tabla";
+            this.tabla.RowHeadersWidth = 51;
+            this.tabla.RowTemplate.Height = 24;
+            this.tabla.Size = new System.Drawing.Size(834, 484);
+            this.tabla.TabIndex = 2;
+            this.tabla.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tabla_CellContentClick);
+            this.tabla.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.tabla_CellEndEdit);
+            // 
+            // Hora
+            // 
+            this.Hora.HeaderText = "Hora";
+            this.Hora.MinimumWidth = 6;
+            this.Hora.Name = "Hora";
+            this.Hora.ReadOnly = true;
+            this.Hora.Width = 50;
+            // 
+            // Actividades
+            // 
+            this.Actividades.HeaderText = "Actividades";
+            this.Actividades.MinimumWidth = 6;
+            this.Actividades.Name = "Actividades";
+            this.Actividades.Width = 500;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 770);
+            this.Controls.Add(this.tabla);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView tabla;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Actividades;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestion_de_tareas
+{
+    public partial class Form1 : Form
+    {
+        const int filas = 96; // 96 filas de 15 minutos tiene cada día
+
+        public Form1()
+        {
+            InitializeComponent();
+            //no permitimos que la el formulario cambie de tamaño ni que se maximice
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Añadimos las filas a la tabla
+            for (int i = 0; i < filas; i++)
+            {
+                tabla.Rows.Add();
+            }
+
+            CargarFecha();
+        }
+
+        private void CargarFecha()
+        {
+            DateTime fechaSeleccionada = monthCalendar1.SelectionStart; //Obtenemos la fecha seleccionada en el calendario
+            label1.Text = "Fecha seleccionada: " + fechaSeleccionada.ToString("dd/MM/yyyy");
+            //Guardamos la fecha en un string
+            string fecha = fechaSeleccionada.Day.ToString() + fechaSeleccionada.Month.ToString() + fechaSeleccionada.Year.ToString();
+            //string fecha = label1.Text;
+
+            if (!File.Exists(fecha))
+            {
+                StreamWriter sw = new StreamWriter(fecha);
+                DateTime hoy = DateTime.Today;
+
+                for (int i = 1; i <= filas; i++)
+                {
+                    sw.WriteLine(hoy.ToString("HH:mm"));
+                    sw.WriteLine("");
+                    hoy = hoy.AddMinutes(15);
+                }
+                sw.Close();
+            }
+            //Leemos el archivo fecha:
+            StreamReader streamReader = new StreamReader(fecha);
+
+            int cont = 0;
+
+            while (!streamReader.EndOfStream)
+            {
+                string linea_1 = streamReader.ReadLine(); 
+                string linea_2 = streamReader.ReadLine();
+
+                tabla.Rows[cont].Cells[0].Value = linea_1;
+                tabla.Rows[cont].Cells[1].Value = linea_2;
+
+                cont++;
+            }
+            streamReader.Close();
+        }
+
+        private void tabla_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e) 
+        {
+            //Cuando seleccionamos una nueva fecha, la cargamos:
+            CargarFecha();
+        }
+
+        private void tabla_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            //Esta función se ejecuta cuando termine de editar una actividad
+            //Actualizamos el archivo de texto de la fecha seleccionada
+
+            DateTime fechaSeleccionada = monthCalendar1.SelectionStart; //Obtenemos la fecha seleccionada en el calendario
+            //Guardamos la fecha en un string
+            string fecha = fechaSeleccionada.Day.ToString() + fechaSeleccionada.Month.ToString() + fechaSeleccionada.Year.ToString();
+
+            //Sobreescribimos el archivo de la fecha seleccionada para poder añadir la nueva tarea
+            //Recorremos toda la tabla y la grabamos en esta fecha:
+            StreamWriter sw = new StreamWriter(fecha);
+            for (int i = 0; i < tabla.Rows.Count; i++)
+            {
+                sw.WriteLine(tabla.Rows[i].Cells[0].Value.ToString()); //Grabamos la hora
+                //Si no hay una tarea a esa hora, la grabamos en la columna de actividades:
+                if (tabla.Rows[i].Cells[1].Value != null)
+                {
+                    sw.WriteLine(tabla.Rows[i].Cells[1].Value.ToString());
+                }
+                else
+                {
+                    sw.WriteLine(""); //Si no hay una actividad, grabamos una linea en blanco.
+                }
+            }
+            sw.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
